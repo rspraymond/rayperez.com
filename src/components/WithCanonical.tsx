18b6a9d5aee8c99d591723159b8e6a39308c7099
Raymond Perez <ray@rayperez.com>
2024-07-08 06:32:50 -0600

    feat: add canonical urls (#5)

---
+++ src/components/WithCanonical.tsx
@@ -0,0 +1,27 @@
+import { ComponentType } from 'react'
+import { Helmet } from 'react-helmet'
+
+/**
+ * Higher-order component (HOC) for adding a canonical link to a component.
+ *
+ * This HOC wraps a given React component and injects a `<link rel='canonical'>` element
+ * into the document head using the `react-helmet` library. This is useful for SEO purposes,
+ * indicating to web crawlers the preferred URL for the content being rendered by the component.
+ *
+ * @template P - The props type of the wrapped component.
+ * @param {ComponentType<P>} Component - The React component to wrap.
+ * @param {string} canonicalUrl - The URL to be used as the value for the `href` attribute in the canonical link.
+ * @returns A React functional component that renders the wrapped component along with the canonical link.
+ */
+const withCanonical = <P extends object>(Component: ComponentType<P>, canonicalUrl: string) => {
+  return (props: P) => (
+    <>
+      <Helmet>
+        <link rel='canonical' href={canonicalUrl} />
+      </Helmet>
+      <Component {...props} />
+    </>
+  )
+}
+
+export default withCanonical
