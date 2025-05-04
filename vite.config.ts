162a2e2fbfc3325bd415b0f13f2cd2a479163b99
Raymond Perez <ray@rayperez.com>
2025-05-04 07:25:21 -0600

    chore: upgrade to vite 6 (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    warmup: {
+      // Preload the entry points
+      clientFiles: ['./src/main.tsx'],
+    },
+  },
   build: {
     rollupOptions: {
       output: {
