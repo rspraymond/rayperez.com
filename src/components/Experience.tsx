31e8a57c6bed11d3206725952050cd14997c9884
Raymond Perez <ray@rayperez.com>
2025-06-22 07:49:41 -0600

    feat: add seo experience schema (#54)

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,8 @@
 import { Box, Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
+import { helmetJsonLdProp } from 'react-schemaorg'
+import { Occupation } from 'schema-dts'
+import { Helmet } from 'react-helmet'
 
 type Experience = {
   title: string
@@ -9,20 +12,32 @@ type Experience = {
 }
 
 const Experience: React.FC<Experience> = ({ title, company, duration, bullets }) => (
-  <Card style={{ marginTop: 16 }}>
-    <CardContent>
-      <Typography variant='h5' gutterBottom>
-        {title}
-      </Typography>
-      <Typography variant='subtitle1' color='textSecondary'>
-        {company} - {duration}
-      </Typography>
-      {bullets.map((bullet, key) => (
-        <Box marginTop={2} key={key}>
-          <Typography variant='body1'>{bullet}</Typography>
-        </Box>
-      ))}
-    </CardContent>
-  </Card>
+  <React.Fragment>
+    <Helmet
+      script={[
+        helmetJsonLdProp<Occupation>({
+          '@context': 'https://schema.org',
+          '@type': 'Occupation',
+          name: title,
+          responsibilities: bullets.join(' '),
+        }),
+      ]}
+    />
+    <Card style={{ marginTop: 16 }}>
+      <CardContent>
+        <Typography variant='h5' gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant='subtitle1' color='textSecondary'>
+          {company} - {duration}
+        </Typography>
+        {bullets.map((bullet, key) => (
+          <Box marginTop={2} key={key}>
+            <Typography variant='body1'>{bullet}</Typography>
+          </Box>
+        ))}
+      </CardContent>
+    </Card>
+  </React.Fragment>
 )
 export default Experience
