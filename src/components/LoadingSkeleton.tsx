090ec32db5f8caacca9a43c79d2a3a9a4913d62b
Raymond Perez <ray@rayperez.com>
2025-07-20 09:35:00 -0600

    feat: lazy load blog post components

---
+++ src/components/LoadingSkeleton.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, Skeleton } from '@mui/material'
+
+interface LoadingSkeletonProps {
+  testId?: string
+}
+
+const LoadingSkeleton: React.FC<LoadingSkeletonProps> = ({ testId }) => {
+  return (
+    <Box sx={{ width: '100%', mb: 2 }} data-testid={testId}>
+      <Skeleton variant='rectangular' height={60} sx={{ mb: 1 }} />
+      <Skeleton variant='text' width='60%' height={24} sx={{ mb: 0.5 }} />
+      <Skeleton variant='text' width='40%' height={24} sx={{ mb: 0.5 }} />
+      <Skeleton variant='text' width='80%' height={24} />
+    </Box>
+  )
+}
+
+export default LoadingSkeleton
