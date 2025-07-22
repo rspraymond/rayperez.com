730cfc586193d5f39c1254acbfb8c9784c51088e
Raymond Perez <ray@rayperez.com>
2025-07-22 05:59:35 -0600

    chore: add unit test for skeleton component (#80)

--- src/components/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton.tsx
@@ -7,7 +7,7 @@ interface LoadingSkeletonProps {
 
 const LoadingSkeleton: React.FC<LoadingSkeletonProps> = ({ testId }) => {
   return (
-    <Box sx={{ width: '100%', mb: 2 }} data-testid={testId}>
+    <Box sx={{ width: '100%', mb: 2 }} data-testid={testId || 'skeleton-container'}>
       <Skeleton variant='rectangular' height={60} sx={{ mb: 1 }} />
       <Skeleton variant='text' width='60%' height={24} sx={{ mb: 0.5 }} />
       <Skeleton variant='text' width='40%' height={24} sx={{ mb: 0.5 }} />
