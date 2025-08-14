d384ed87726caafffe8696ea86b06fb848378b37
Raymond Perez <ray@rayperez.com>
2025-08-14 05:49:44 -0600

    feat: add copy link button

--- src/components/SocialShareButtons.tsx
+++ src/components/SocialShareButtons.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Box, IconButton, Tooltip } from '@mui/material'
+import { Box, IconButton, Tooltip, Snackbar } from '@mui/material'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import FacebookIcon from '@mui/icons-material/Facebook'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 
 interface SocialShareButtonsProps {
   title: string
@@ -25,42 +26,68 @@ const getShareUrl = (platform: 'twitter' | 'linkedin' | 'facebook', title: strin
 
 const SocialShareButtons: React.FC<SocialShareButtonsProps> = ({ title }) => {
   const url = typeof window !== 'undefined' ? window.location.href : ''
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false)
 
   const handleShare = (platform: 'twitter' | 'linkedin' | 'facebook') => {
     const shareUrl = getShareUrl(platform, title, url)
     window.open(shareUrl, '_blank', 'noopener,noreferrer')
   }
 
+  const handleCopy = () => {
+    if (typeof navigator !== 'undefined' && navigator.clipboard && url) {
+      navigator.clipboard.writeText(url)
+      setSnackbarOpen(true)
+    }
+  }
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false)
+  }
+
   return (
-    <Box display='flex' gap={1} mb={2}>
-      <Tooltip title='Share on Twitter'>
-        <IconButton
-          aria-label='Share on Twitter'
-          onClick={() => handleShare('twitter')}
-          size='small'
-        >
-          <TwitterIcon fontSize='small' />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title='Share on LinkedIn'>
-        <IconButton
-          aria-label='Share on LinkedIn'
-          onClick={() => handleShare('linkedin')}
-          size='small'
-        >
-          <LinkedInIcon fontSize='small' />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title='Share on Facebook'>
-        <IconButton
-          aria-label='Share on Facebook'
-          onClick={() => handleShare('facebook')}
-          size='small'
-        >
-          <FacebookIcon fontSize='small' />
-        </IconButton>
-      </Tooltip>
-    </Box>
+    <>
+      <Box display='flex' gap={1} mb={2}>
+        <Tooltip title='Copy link'>
+          <IconButton aria-label='Copy link' onClick={handleCopy} size='small'>
+            <ContentCopyIcon fontSize='small' />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title='Share on Twitter'>
+          <IconButton
+            aria-label='Share on Twitter'
+            onClick={() => handleShare('twitter')}
+            size='small'
+          >
+            <TwitterIcon fontSize='small' />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title='Share on LinkedIn'>
+          <IconButton
+            aria-label='Share on LinkedIn'
+            onClick={() => handleShare('linkedin')}
+            size='small'
+          >
+            <LinkedInIcon fontSize='small' />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title='Share on Facebook'>
+          <IconButton
+            aria-label='Share on Facebook'
+            onClick={() => handleShare('facebook')}
+            size='small'
+          >
+            <FacebookIcon fontSize='small' />
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Snackbar
+        open={snackbarOpen}
+        onClose={handleSnackbarClose}
+        autoHideDuration={1500}
+        message='Link copied'
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      />
+    </>
   )
 }
 
