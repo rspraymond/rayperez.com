0d4c0405322e2c4c55e3ea536a42dc651017bc31
Raymond Perez <ray@rayperez.com>
2025-10-11 07:07:57 -0600

    fix: refactor acheivements component to use section card

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -1,33 +1,19 @@
-import { Box, Card, CardContent, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined'
+import SectionCard from './SectionCard'
 
 type Achievements = {
   achievements: string[]
 }
 
 const Achievements: React.FC<Achievements> = ({ achievements }) => (
-  <Card style={{ marginTop: 16 }}>
-    <CardContent>
-      <Typography
-        variant='h5'
-        gutterBottom
-        component='h2'
-        sx={{ display: 'flex', alignItems: 'center', fontWeight: 600 }}
-      >
-        <EmojiEventsOutlinedIcon
-          sx={{ mr: 1, color: 'text.secondary' }}
-          fontSize='medium'
-          aria-hidden='true'
-        />
-        Achievements
-      </Typography>
-      {achievements.map((achievement, key) => (
-        <Box marginTop={2} key={key}>
-          <Typography variant='body1'>{achievement}</Typography>
-        </Box>
-      ))}
-    </CardContent>
-  </Card>
+  <SectionCard title='Achievements' icon={EmojiEventsOutlinedIcon}>
+    {achievements.map((achievement, key) => (
+      <Box marginTop={2} key={key}>
+        <Typography variant='body1'>{achievement}</Typography>
+      </Box>
+    ))}
+  </SectionCard>
 )
 export default Achievements
