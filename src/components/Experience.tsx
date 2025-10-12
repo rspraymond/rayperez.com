6eac6b308ca3bf6648248cc9dad1058226b9d5f6
Raymond Perez <ray@rayperez.com>
2025-10-12 08:21:00 -0600

    fix: refactor experience component to use section card

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,10 @@
-import { Box, Card, CardContent, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import { helmetJsonLdProp } from 'react-schemaorg'
 import { Occupation } from 'schema-dts'
 import { Helmet } from 'react-helmet'
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline'
+import SectionCard from './SectionCard'
 
 type Experience = {
   title: string
@@ -24,31 +25,16 @@ const Experience: React.FC<Experience> = ({ title, company, duration, bullets })
         }),
       ]}
     />
-    <Card style={{ marginTop: 16 }}>
-      <CardContent>
-        <Typography
-          variant='h5'
-          gutterBottom
-          component='h2'
-          sx={{ display: 'flex', alignItems: 'center', fontWeight: 600 }}
-        >
-          <WorkOutlineIcon
-            sx={{ mr: 1, color: 'text.secondary' }}
-            fontSize='medium'
-            aria-hidden='true'
-          />
-          {title}
-        </Typography>
-        <Typography variant='subtitle1' color='textSecondary' component='p'>
-          {company} - {duration}
-        </Typography>
-        {bullets.map((bullet, key) => (
-          <Box marginTop={2} key={key}>
-            <Typography variant='body1'>{bullet}</Typography>
-          </Box>
-        ))}
-      </CardContent>
-    </Card>
+    <SectionCard title={title} icon={WorkOutlineIcon}>
+      <Typography variant='subtitle1' color='textSecondary' component='p'>
+        {company} - {duration}
+      </Typography>
+      {bullets.map((bullet, key) => (
+        <Box marginTop={2} key={key}>
+          <Typography variant='body1'>{bullet}</Typography>
+        </Box>
+      ))}
+    </SectionCard>
   </React.Fragment>
 )
 export default Experience
